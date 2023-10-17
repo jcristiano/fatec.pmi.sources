31ba7fc2fb6b8057a996ce8f2a5aa7b98ab2681f
Cristiano <jcristiano.c@gmail.com>
2023-10-16 22:06:57 -0300

    Ajustes gerais

--- csharp-wf-hello-world/HelloWorld/HelloWorld/Program.cs
+++ csharp-wf-hello-world/HelloWorld/HelloWorld/Program.cs
@@ -16,7 +16,7 @@ namespace HelloWorld
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmPrincipal());
+            Application.Run(new frmCapturaKey());
         }
     }
 }
